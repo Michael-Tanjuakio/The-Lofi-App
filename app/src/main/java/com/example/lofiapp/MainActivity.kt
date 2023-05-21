d5a04beb0b36e79d261319404bd7d33f2fa130a0
akomikee <miketan843@gmail.com>
2023-05-20 22:07:50 -0500

    topbar and searchbar

--- app/src/main/java/com/example/lofiapp/MainActivity.kt
+++ app/src/main/java/com/example/lofiapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.lofiapp
 
+import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.fontResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lofiapp.ui.theme.LofiappTheme
+import com.example.lofiapp.ui.theme.flamenco_regular
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +29,12 @@ class MainActivity : ComponentActivity() {
             LofiappTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    displayScreen(
+                        stringResource(R.string.app_name),
+                        stringResource(R.string.Search)
+                    )
                 }
             }
         }
@@ -30,14 +42,46 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun displayScreen(title: String, search: String) {
+    var text by remember { mutableStateOf("")}
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TopAppBar(
+            title = {
+                Text(text = title, color = Color.White, fontFamily = flamenco_regular, fontSize = 32.sp)
+            },
+            backgroundColor = Color(0xFF24CAAC)
+        )
+
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LofiappTheme {
-        Greeting("Android")
+        displayScreen(
+            stringResource(R.string.app_name),
+            stringResource(R.string.Search)
+        );
     }
 }
+
+/*
+        TextField(
+            placeholder = {
+                Text(text = search,
+                    fontFamily = flamenco_regular,
+                    fontSize = 16.sp)
+                          },
+            value = text,
+            onValueChange = { text = it },
+            modifier = Modifier
+                .align(alignment = Alignment.CenterHorizontally)
+                .padding(top = 48.dp),
+            singleLine = true
+        )
+*/
--- app/src/main/java/com/example/lofiapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/lofiapp/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.lofiapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.lofiapp.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -26,3 +28,5 @@ val Typography = Typography(
     )
     */
 )
+
+val flamenco_regular = FontFamily(Font(R.font.flamenco_regular))
