cf78ac08b8be68105c7d9d3f2d21745e44c8aceb
LauraVillarr <lauravillarr1@gmail.com>
2023-05-24 22:37:59 -0500

    reformatted file structure

---
+++ app/src/main/java/com/example/lofiapp/domain/ViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.lofiapp.domain
+
+class ViewModel {
+}
--- app/src/main/java/com/example/lofiapp/screens/MainActivity.kt
+++ app/src/main/java/com/example/lofiapp/screens/MainActivity.kt
@@ -1,24 +1,18 @@
-package com.example.lofiapp
+package com.example.lofiapp.screens
 
-import android.graphics.fonts.Font
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.lofiapp.R
 import com.example.lofiapp.ui.theme.LofiappTheme
 import com.example.lofiapp.ui.theme.flamenco_regular
 
